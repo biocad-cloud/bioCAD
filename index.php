fe2b23564a9d5b321786712650d50f4d351ff0b9
xieguigang <xie.guigang@gmail.com>
2017-02-27 18:23:49 +0800

    success on listing all commands

--- index.php
+++ index.php
@@ -1,4 +1,19 @@
+<html>
+<header>
+<title>GCModeller Commands</title>
+</header>
+<body>
+
+<h1>Welcomes to the GCModeller Cloud platform</h1>
+
+<hr>
 <?php
-    $output = shell_exec('gcmodeller ?');
+
+    /*
+        list all of the avaliable GCModeller commandline tools 
+    */
+    $output = shell_exec('gcmodeller --ls');
     echo "<pre>$output</pre>";
 ?>
+</body>
+</html>
