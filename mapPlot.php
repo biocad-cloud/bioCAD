03a35bee53cf09522cc1b70cd4aa4bcbaaf44708
xieguigang <xie.guigang@gmail.com>
2017-02-28 11:27:47 +0800

    map plot shell

---
+++ mapPlot.php
@@ -0,0 +1,18 @@
+<html>
+<header>
+<title>GCModeller chromosome map plot</title>
+</header>
+<body>
+
+<h1>GCModeller chromosome map plot API</h1>
+
+<hr />
+
+<form method="POST" action="./plot.php">
+	
+	size: (<input type="text" name="width" /> x <input type="text" name="height" />)<br />
+	genome: <input type="file" name="genbank" />
+</form>
+
+</body>
+</html>
