ddb299a6c662d45d29d29847d8c4a73de5b05576
xieguigang <xie.guigang@gmail.com>
2017-03-01 07:26:23 +0800

    strings split??

--- lib/Strings.php
+++ lib/Strings.php
@@ -2,9 +2,16 @@
 
 /* The strings module functions in VisualBasic language */
 
-Class Strings {
+class Strings {
 	
-	
+	public static function Split($str, $deli) {
+		
+// split the phrase by any number of commas or space characters,
+// which include " ", \r, \t, \n and \f
+$words = preg_split("/" . $deli . "/", $str);
+return $words;
+
+	}
 	
 }
 
