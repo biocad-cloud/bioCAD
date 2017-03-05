67ee3d9de76e31388a15882aa9a6ade2e85faa12
xieguigang <xie.guigang@gmail.com>
2017-03-05 19:46:06 +0800

    conversion tools

---
+++ lib/Conversion.php
@@ -0,0 +1,27 @@
+<?php 
+
+class Conversion {
+
+    public static function CInt($str) {
+        return intval($str);
+    }
+
+    public static function CDbl($str) {
+
+    }
+
+    public static function CBool($str) {
+
+    }
+
+    public static function CStr($str) {
+
+    }
+
+    public static function CSng($str) {
+        
+    }
+
+}
+
+?>
