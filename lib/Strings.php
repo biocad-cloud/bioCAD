6b7d331475e5b647a921acfad254017d51419537
xieguigang <xie.guigang@gmail.com>
2017-03-05 19:38:16 +0800

    instr

--- lib/Strings.php
+++ lib/Strings.php
@@ -13,6 +13,19 @@ return $words;
 
 	}
 	
+// 如果查找不到字串在目标字符串之上的位置，则函数返回0
+// 假若能够查找得到，则会返回以1为准的位置
+	public static function InStr($str, $find_subString, $begin) {
+
+$p = strpos($str, $find_subString, $begin)
+
+if ($p === false) {
+	return 0;
+} else {
+	return ($p + $begine + 1);
+}
+
+	}
 }
 
 ?>
