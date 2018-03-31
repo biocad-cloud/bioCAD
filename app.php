d7ba3a534ee17efad3658841c7b3bbcf7405b055
谢桂纲 <xie.guigang@gcmodeller.org>
2018-03-31 14:34:19 +0800

    add html views

--- app.php
+++ app.php
@@ -1,5 +1,14 @@
 <?php
 
 # Module file for handling HTML user interface
+include "./mod/dotnet/package.php";
 
+dotnet::AutoLoad("./etc/config.php");
+dotnet::HandleRequest(new app());
+// dotnet::printMySqlTransaction();
+dotnet::writeMySqlLogs(TRUE);
+
+class app {
+
+}
 ?>
