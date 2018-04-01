abf41d68f01ad3b9245ebdee991be32e19587238
谢桂纲 <xie.guigang@gcmodeller.org>
2018-04-01 19:55:37 +0800

    config data

--- etc/config.php
+++ etc/config.php
@@ -1,6 +1,19 @@
 <?php
 
 // 网站的配置数据
-    return array();
+    return array(    
+        'DB_TYPE' => 'mysql',
+        'DB_HOST' => '127.0.0.1',
+        'DB_NAME' => 'metacardio',
+        'DB_USER' => 'metacardio',
+        'DB_PWD'  => '1234',
+        'DB_PORT' => '3306',
+        // 'DB_PREFIX' => 'xcms_',
+        //密钥
+        "AUTHCODE" => 'A2f0qS78ttR9HpqeOT',
+        //cookies
+        "COOKIE_PREFIX" => '8FSUix_',
+        "ERR_HANDLER_DISABLE" => "FALSE"
+    );
 
 ?>
