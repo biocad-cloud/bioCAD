ba20a45c9ea5caef8336ea2769da22c76068a609
xieguigang <xie.guigang@gcmodeller.org>
2018-04-01 15:11:51 +0800

    load user settings

--- api.php
+++ api.php
@@ -3,6 +3,8 @@
 # Module file for handling biostack data analysis API
 include "./mod/dotnet/package.php";
 
+Imports("Microsoft.VisualBasic.Strings");
+
 dotnet::AutoLoad("./etc/config.php");
 dotnet::HandleRequest(new api());
 // dotnet::printMySqlTransaction();
@@ -10,6 +12,52 @@ dotnet::writeMySqlLogs(TRUE);
 
 class api {
 
+    const CredentialVerifyError = "User not found or invalid credential information provided!";
+
+    // 用户登录验证
+    public function login() {
+
+        // {account/email, password_md5}
+        $credential = $_POST;
+        $account = $credential["account"];
+        $assert = "";
+
+        if (Strings::InStr($account, "@") > 0) {
+            # 是电子邮件来的？
+            $assert = "lower(`email`)   = lower('$account')";
+        } else {
+            $assert = "lower(`account`) = lower('$account')";
+        }
+
+        // account或者account都应该是唯一的，所以在这里查找出来的用户信息也应该是唯一的
+        $user = (new Table("user"))->where($assert)->find();
+
+        if (!$user || $user["password"] != $credential["password"]) {
+
+            # 没有找到对应的记录
+            echo dotnet::errorMsg(self::CredentialVerifyError);
+            die;
+
+        } else {
+
+            // 向session之中写入用户的记录信息，然后返回成功消息
+            // 从数据库之中读取用户的配置数据信息
+            $settings = (new Table("user_settings"))->where(array("user_id" => $user["id"]))->find();
+
+            echo dotnet::successMsg("success");
+
+        }
+    }
+
+    // 允许用户进行匿名登陆，使用cookie来记录session的编号而获取服务器的计算资源
+    // 在这个函数之中为用户初始化后台的session的数据记录信息
+    public function anonymous_login() {
+
+    }
+
+    public function logout() {
+
+    }
 }
 
 ?>
--- app.php
+++ app.php
@@ -10,8 +10,40 @@ dotnet::writeMySqlLogs(TRUE);
 
 class app {
 
+	/*
+		Session之中所保存的临时信息的数据结构大致如下：
+
+		{
+			user: {
+				uid, name, email, role
+			}, 
+			settings: {
+				anonymous_warning, notify_config
+			}
+		}
+	
+	*/
+
 	// Load user info from php session
 	private function userInfo() {
+		// 用户的标识信息是从服务器后台的session数据之中读取出来的
+		// 如果客户是匿名的非注册客户，则当浏览器被清除掉cookie之后，
+		// session关联信息将会丢失
+		// 而注册用户则可以依靠数据库之中的唯一标识符来保持记录状态
+		$user = $_SESSION["user"];
+
+		return $user;
+	}
+
+	// 获取当前的用户的配置信息
+	public function getMySettings() {
+		$config = $_SESSION["settings"];
+		$config = json_encode($config);
+
+		echo $config;
+	}
+
+	public function login() {
 
 	}
 
@@ -25,10 +57,17 @@ class app {
 
     /**
 	 * 用户设置中心
-	 * 
 	 */
 	public function me() {
-		view::Display(array("title" => "My Settings"));
+
+		// 首先需要从session之中读取在服务器后台所保存的临时信息
+		// 
+		$user          = $this->userInfo();
+		$user["title"] = "My Settings";
+
+
+
+		view::Display($user);
 	}
 }
 
