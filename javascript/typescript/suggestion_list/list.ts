00926b7ea552abe95c7122f556c93c87633ded0d
xieguigang <xie.guigang@gcmodeller.org>
2018-06-15 20:30:32 +0800

    leven script from https://github.com/sindresorhus/leven/blob/master/index.js

---
+++ javascript/typescript/suggestion_list/list.ts
@@ -0,0 +1,24 @@
+﻿
+/**
+ * Term for suggestion
+*/
+class term {
+
+    /**
+     * 这个term在数据库之中的id编号
+    */
+    public id: number;
+    public term: string;
+
+    constructor(id: number, term: string) {
+        this.id = id;
+        this.term = term;
+    }
+
+    /**
+     * 使用动态规划算法计算出当前的这个term和用户输入之间的相似度
+    */
+    public dist(input: string): number {
+
+    }
+}
