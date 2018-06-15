e8b488a8401200aeb5e52b87a005e52d68441c13
xieguigang <xie.guigang@gcmodeller.org>
2018-06-16 00:19:39 +0800

    make suggestions

---
+++ javascript/typescript/suggestion_list/display.ts
@@ -0,0 +1,40 @@
+﻿// 将结果显示到网页上面
+
+function makeSuggestions(terms: term[], div: string, click: (term: term) => void): (input: string) => void {
+    var suggestions: suggestion = new suggestion(terms);
+
+    return (input: string) => {
+        showSuggestions(suggestions, input, div, click);
+    };
+}
+
+function showSuggestions(suggestion: suggestion, input: string, div: string, click: (term: term) => void) {
+    var node = document.getElementById(div);
+
+    if (!node) {
+        console.error(`Unable to find node which its id equals to: ${div}`);
+        return;
+    } else {
+        node.innerHTML = "";
+    }
+
+    suggestion.populateSuggestion(input).forEach(term => {
+        node.appendChild(listItem(term, click));
+    });
+}
+
+function listItem(term: term, click: (term: term) => void): HTMLElement {
+    var div = document.createElement("div");
+    var a = document.createElement("a");
+
+    a.href = "#";
+    a.onclick = () => {
+        click(term);
+    }
+    a.text = term.term;
+    a.title = term.term;
+
+    div.appendChild(a);
+
+    return div;
+}
