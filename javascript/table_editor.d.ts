6d1fc46ab6ab05fb12ac1dd89d825d9bc1b4b7b7
xieguigang <xie.guigang@gcmodeller.org>
2018-06-18 03:43:55 +0800

    add type export

---
+++ javascript/table_editor.d.ts
@@ -0,0 +1,35 @@
+declare class editor {
+    private td;
+    private tr;
+    private tbody;
+    private table;
+    static readonly editor_template: string;
+    constructor(tr: HTMLElement, tbody: HTMLElement, table: tableEditor);
+    getElementById(id: string): HTMLElement;
+    show(id: string): void;
+    hide(id: string): void;
+    hideInputs(): void;
+    showInputs(): void;
+    confirmNew(): void;
+    cancelAddNew(): void;
+    removeCurrent(): void;
+    editThis(): void;
+    confirmEdit(): void;
+}
+declare class tableEditor {
+    private headers;
+    private rowNumbers;
+    private tbody;
+    private showRowNumber;
+    edit_lock: boolean;
+    warningEditLock: () => void;
+    /**
+     * 这个构造函数将会创建一个新的table对象
+     *
+     * @param id id value of a <div> tag.
+    */
+    constructor(headers: string[], id: string, style?: string, className?: string, tdWidth?: string[], warning?: () => void, showRowNumber?: boolean);
+    addNew(): editor;
+    private addNewInternal();
+    TableData(): Object[];
+}
