0033c71069b34a65f1118200a96157afcac02c7a
xieguigang <xie.guigang@gcmodeller.org>
2018-06-18 04:57:58 +0800

    login script

--- javascript/login_script.d.ts
+++ javascript/login_script.d.ts
@@ -1,2 +1,14 @@
-declare module "server" {
+declare namespace login_script {
+    class user {
+        account: string;
+        password: string;
+        constructor(account: string, password: string, md5: (input: string) => string);
+        login(api: string): msg;
+    }
+    class msg {
+        code: number;
+        msg: string;
+        url: string;
+        readonly success: boolean;
+    }
 }
---
+++ typescript/login_script/user.ts
@@ -0,0 +1,40 @@
+﻿namespace login_script {
+
+    export class user {
+
+        public account: string;
+        public password: string;
+
+        constructor(account: string, password: string, md5: (input: string) => string) {
+            this.account = account;
+            this.password = md5(password);
+        }
+
+        public login(api: string): msg {
+            var xhr = new XMLHttpRequest();
+
+            xhr.open("POST", api, false);
+            xhr.setRequestHeader("Content-Type", "application/json");
+            xhr.send(JSON.stringify({
+                login: this.account,
+                password: this.password
+            }));
+
+            var response = xhr.responseText;
+            var data: msg = JSON.parse(response);
+
+            return data;
+        }
+    }
+
+    export class msg {
+
+        public code: number;
+        public msg: string;
+        public url: string;
+
+        public get success(): boolean {
+            return this.code == 0;
+        }
+    }
+}
