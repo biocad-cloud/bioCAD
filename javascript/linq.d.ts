1295d401fc162ce7032b3c8f3e124de66cd07834
xieguigang <xie.guigang@gcmodeller.org>
2018-08-19 00:25:33 +0800

    update linq api

--- javascript/linq.d.ts
+++ javascript/linq.d.ts
@@ -226,58 +226,77 @@ declare class NumericRange implements DoubleRange {
     toString(): string;
 }
 /**
- * 用于进行数据分组所需要的最基础的二叉树数据结构
+ * Binary tree implements
 */
-declare class binaryTree<T, V> {
+declare namespace algorithmBTree {
     /**
-     * 根节点，根节点的key值可能会对二叉树的构建造成很大的影响
+     * 用于进行数据分组所需要的最基础的二叉树数据结构
     */
-    root: node<T, V>;
-    /**
-     * 这个函数指针描述了如何对两个``key``之间进行比较
-     *
-     * 返回结果值：
-     *
-     * + ``等于0`` 表示二者相等
-     * + ``大于0`` 表示a大于b
-     * + ``小于0`` 表示a小于b
-    */
-    compares: (a: T, b: T) => number;
-    /**
-     * 构建一个二叉树对象
-     *
-     * @param comparer 这个函数指针描述了如何进行两个对象之间的比较操作，如果这个函数参数使用默认值的话
-     *                 则只能够针对最基本的数值，逻辑变量进行操作
-    */
-    constructor(comparer?: (a: T, b: T) => number);
-    /**
-     * 向这个二叉树对象之中添加一个子节点
-    */
-    add(term: T, value?: V): void;
-    /**
-     * 根据key值查找一个节点，然后获取该节点之中与key所对应的值
-     *
-     * @returns 如果这个函数返回空值，则表示可能未找到目标子节点
-    */
-    find(term: T): V;
-    /**
-     * 将这个二叉树对象转换为一个节点的数组
-    */
-    ToArray(): node<T, V>[];
-    /**
-     * 将这个二叉树对象转换为一个Linq查询表达式所需要的枚举器类型
-    */
-    AsEnumerable(): IEnumerator<node<T, V>>;
+    class binaryTree<T, V> {
+        /**
+         * 根节点，根节点的key值可能会对二叉树的构建造成很大的影响
+        */
+        root: node<T, V>;
+        /**
+         * 这个函数指针描述了如何对两个``key``之间进行比较
+         *
+         * 返回结果值：
+         *
+         * + ``等于0`` 表示二者相等
+         * + ``大于0`` 表示a大于b
+         * + ``小于0`` 表示a小于b
+        */
+        compares: (a: T, b: T) => number;
+        /**
+         * 构建一个二叉树对象
+         *
+         * @param comparer 这个函数指针描述了如何进行两个对象之间的比较操作，如果这个函数参数使用默认值的话
+         *                 则只能够针对最基本的数值，逻辑变量进行操作
+        */
+        constructor(comparer?: (a: T, b: T) => number);
+        /**
+         * 向这个二叉树对象之中添加一个子节点
+        */
+        add(term: T, value?: V): void;
+        /**
+         * 根据key值查找一个节点，然后获取该节点之中与key所对应的值
+         *
+         * @returns 如果这个函数返回空值，则表示可能未找到目标子节点
+        */
+        find(term: T): V;
+        /**
+         * 将这个二叉树对象转换为一个节点的数组
+        */
+        ToArray(): node<T, V>[];
+        /**
+         * 将这个二叉树对象转换为一个Linq查询表达式所需要的枚举器类型
+        */
+        AsEnumerable(): IEnumerator<node<T, V>>;
+    }
 }
-declare module binaryTreeExtensions {
-    function populateNodes<T, V>(tree: node<T, V>): node<T, V>[];
+declare namespace algorithmBTree {
+    /**
+     * data extension module for binary tree nodes data sequence
+    */
+    module binaryTreeExtensions {
+        /**
+         * Convert a binary tree object as a node array.
+        */
+        function populateNodes<T, V>(tree: node<T, V>): node<T, V>[];
+    }
 }
-declare class node<T, V> {
-    key: T;
-    value: V;
-    left: node<T, V>;
-    right: node<T, V>;
-    constructor(key: T, value?: V, left?: node<T, V>, right?: node<T, V>);
+declare namespace algorithmBTree {
+    /**
+     * A binary tree node.
+    */
+    class node<T, V> {
+        key: T;
+        value: V;
+        left: node<T, V>;
+        right: node<T, V>;
+        constructor(key: T, value?: V, left?: node<T, V>, right?: node<T, V>);
+        toString(): string;
+    }
 }
 /**
  * http://www.rfc-editor.org/rfc/rfc4180.txt
@@ -436,6 +455,9 @@ declare module Enumerable {
     function SkipWhile<T>(source: T[], predicate: (e: T) => boolean): IEnumerator<T>;
     function All<T>(source: T[], predicate: (e: T) => boolean): boolean;
     function Any<T>(source: T[], predicate: (e: T) => boolean): boolean;
+    /**
+     * Implements a ``group by`` operation by binary tree data structure.
+    */
     function GroupBy<T, TKey>(source: T[], getKey: (e: T) => TKey, compares: (a: TKey, b: TKey) => number): IEnumerator<Group<TKey, T>>;
 }
 declare class Group<TKey, T> extends IEnumerator<T> {
