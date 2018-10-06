2f963e703bcf3535d2f58d849e9d7d1007670427
xieguigang <xie.guigang@gcmodeller.org>
2018-10-06 23:26:46 +0800

    update app version settings

--- etc/config.php
+++ etc/config.php
@@ -19,6 +19,7 @@ return [
     "RFC7231" => "html/http_errors",
     "CACHE"   => true,
     "APP_NAME" => "bioCAD",
+	"APP_VERSION" => "0.222.56-alpha",
     "MVC_VIEW_ROOT" => [
         "passport" => "./html/user/",
         "app" => "./html/Application/"
