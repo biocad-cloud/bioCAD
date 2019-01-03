eb5b85beee53c6a8f9aa3f116f2b05e19312114f
xieguigang <xie.guigang@gcmodeller.org>
2019-01-03 20:44:31 +0800

    login and register script

--- modules/build/biocad.d.ts
+++ modules/build/biocad.d.ts
@@ -1,25 +1,9 @@
 /// <reference path="linq.d.ts" />
-declare namespace login_script {
-    class user {
-        account: string;
-        password: string;
-        constructor(account: string, password: string, md5: (input: string) => string);
-        login(api: string): msg;
+declare namespace bioCAD.WebApp {
+    class LogInScript extends Bootstrap {
+        readonly appName: string;
+        protected init(): void;
+        static login(): void;
+        static register(): void;
     }
-    class msg {
-        code: number;
-        msg: string;
-        url: string;
-        readonly success: boolean;
-    }
-    class registerDiv {
-        username: string;
-        email: string;
-        password: string;
-        checkTermsOfuse: string;
-        constructor(username: string, email: string, password: string, check: string);
-        static DefaultId(): registerDiv;
-    }
-    function login(api: string, md5: (input: string) => string, divId?: registerDiv): msg;
-    function register(api: string, md5: (input: string) => string, divId?: registerDiv): msg;
 }
--- modules/typescript/bioCADweb/WebApp/LogInScript.ts
+++ modules/typescript/bioCADweb/WebApp/LogInScript.ts
@@ -8,6 +8,7 @@
 
         protected init(): void {
             $ts("#login").onclick = LogInScript.login;
+            $ts("#doRegister").onclick = LogInScript.register;
         }
 
         static login() {
@@ -33,48 +34,33 @@
             });
         }
 
-        public static register(
-            api: string,
-            md5: (input: string) => string,
-            divId: registerDiv = registerDiv.DefaultId()): msg {
+        public static register() {
+            var username: string = $ts("#username").CType<HTMLInputElement>().value;
+            var email: string = $ts("#email").CType<HTMLInputElement>().value;
+            var password: string = $ts("#password").CType<HTMLInputElement>().value;
+            var checkTermsOfuse: boolean = $ts("#check_terms_of_use").CType<HTMLInputElement>().checked;
+            var api: string = <any>$ts("@api:register");
 
-            var username: string = (<HTMLInputElement>document.getElementById(divId.username)).value;
-            var email: string = (<HTMLInputElement>document.getElementById(divId.email)).value;
-            var password: string = (<HTMLInputElement>document.getElementById(divId.password)).value;
+            if (!checkTermsOfuse) {
+                (<any>parent).msgbox("You should agree our bioCAD Terms of use at first!", 1);
 
-            if (!(<HTMLInputElement>document.getElementById(divId.checkTermsOfuse)).checked) {
-                var message: msg = new msg();
-
-                message.code = -1;
-                message.url = "#";
-                message.msg = "You should agree our bioCAD Terms of use at first!";
-
-                return message;
             } else if (!username || !email || !password) {
-                var message: msg = new msg();
+                (<any>parent).msgbox("One of the required field is empty!", 1);
 
-                message.code = -2;
-                message.url = "#";
-                message.msg = "One of the required field is empty!";
+            } else {
+                $ts.post(api, {
+                    username: username,
+                    password: md5(password),
+                    email: email
+                }, result => {
 
-                return message;
+                    if (result.code == 0) {
+                        (<any>parent).gotoLogin();
+                    } else {
+                        (<any>parent).msgbox(result.info, 1);
+                    }
+                });
             }
-
-            // 2018-08-12 php后台不识别json的post数据，必须要发送form数据才行？
-            var xhr = new XMLHttpRequest();
-            var formData = new FormData();
-
-            formData.append("username", username);
-            formData.append("password", md5(password));
-            formData.append("email", email);
-
-            xhr.open("POST", api, false);
-            xhr.send(formData);
-
-            var response = xhr.responseText;
-            var data: msg = JSON.parse(response);
-
-            return data;
         }
     }
 }
