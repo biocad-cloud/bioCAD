1733784339f27285cac6e92d117cc68038cf8000
xieguigang <xie.guigang@gcmodeller.org>
2021-12-08 21:26:36 +0800

    Update config.php

--- .etc/config.php
+++ .etc/config.php
@@ -4,26 +4,26 @@
 return [
     'DB_TYPE' => 'mysql',
     'DB_HOST' => '127.0.0.1',
-    'DB_NAME' => 'biocad',
-    'DB_USER' => 'bioCAD',
-    'DB_PWD' => 'biocad.cloud',
+    'DB_NAME' => 'biocad_rds',
+    'DB_USER' => 'biocad_rds',
+    'DB_PWD'  => '12345',
     'DB_PORT' => '3306',
     // 'DB_PREFIX' => 'xcms_',
     //密钥
     "AUTHCODE" => 'A2f0qS78ttR9HpqeOT',
     //cookies
-    "COOKIE_PREFIX" => '8FSUix_',
+    "COOKIE_PREFIX"       => '8FSUix_',
     "ERR_HANDLER_DISABLE" => "FALSE",
 
     // 自定义http错误页面的位置，例如404 500 403等
-    "RFC7231" => APP_PATH . "/views/http_errors",
-    "CACHE"   => true,
-    "APP_NAME" => "bioCAD",
-	"APP_VERSION" => "0.222.58-alpha",
+    "RFC7231"       => APP_PATH . "/views/http_errors",
+    "CACHE"         => true,
+    "APP_NAME"      => "bioCAD",
+	"APP_VERSION"   => "0.222.58-alpha",
     "MVC_VIEW_ROOT" => [
-        "passport" => APP_PATH . "/views/user/",
-        "app" => APP_PATH . "/views/Application/",
-        "index" => APP_PATH . "/views/",
+        "passport"       => APP_PATH . "/views/user/",
+        "app"            => APP_PATH . "/views/Application/",
+        "index"          => APP_PATH . "/views/",
         "pathway_design" => APP_PATH . "/views/Application/pathway_designer/"
     ]
 ];
