76617fb99abf8ab8644f8653d1e91aac17cd6ea0
xieguigang <xie.guigang@gcmodeller.org>
2021-12-09 20:09:03 +0800

    admin page

---
+++ web/platform/admin.php
@@ -0,0 +1,17 @@
+<?php
+
+include __DIR__ . "/../../framework/bootstrap.php";
+
+/**
+ * 系统管理后台界面
+*/
+class app {
+
+    /**
+     * System Administration
+     * 
+    */
+    public function index() {
+        View::Display();
+    }
+}
