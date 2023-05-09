469646248d0b9b75b9bb8e4f7f3f6821c87914d0
xieguigang <xie.guigang@gcmodeller.org>
2023-05-09 21:07:31 +0800

    Update pull.php

--- web/registry/pull.php
+++ web/registry/pull.php
@@ -7,4 +7,30 @@ include __DIR__ . "/../../framework/bootstrap.php";
 */
 class app {
 
+    /**
+     * get taxonomic group data
+     * 
+     * @param string $id the specific taxonomic group id, default null 
+     *    means pull all taxonomic group entries.
+     * 
+     * @uses api
+    */
+    public function taxonomic_group($id = null) {
+        $tax = new Table(["cad_registry" => "taxonomic"]);
+
+        if (Utils::isDbNull($id)) {
+            // pull all
+            controller::success($tax->all());
+        } else {
+            $load = $tax->left_join("genomes")
+            ->on(["genomes" => "taxonomic_group", "taxonomic" => "id"])
+            ->where(["`taxonomic`.`id`" => $id])
+            ->select([
+                "`genomes`.*", "`taxonomic`.`name` as group"
+            ])
+            ;
+
+            controller::success($load, $tax->getLastMySql());
+        }
+    }
 }
